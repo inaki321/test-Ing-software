0bf133fb11c3f4e5361f75dc3d7164b0ba536c27
inaki321 <inakihe00@gmail.com>
2022-11-03 15:27:36 -0600

    counting reps

--- src/pages/test.js
+++ src/pages/test.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useRef } from "react";
 import { AILabWebCam } from "ai-lab";
 import * as poseDetection from "@tensorflow-models/pose-detection";
 import * as tf from "@tensorflow/tfjs";
+import { Conv2D } from "@tensorflow/tfjs";
 
 //conected keypoints of the blazepose
 const BLAZEPOSE_CONNECTED_KEYPOINTS_PAIRS = [
@@ -75,6 +76,16 @@ function Test() {
     //turn skeleton on = true, off =false
     const [skeleton, setSkeleton] = useState(false);
 
+    //posesName
+    const [stand, setStand] = useState('Stand');
+    const [curl, setCurl] = useState('Stand');
+    const [positionIndex, setpositionIndex] = useState(0);
+    const [positionIndex2, setpositionIndex2] = useState(0);
+    const [standReps, setstandReps] = useState(0);
+    const [curlReps, setcurlReps] = useState(0);
+
+    //count 
+
     useEffect(() => {
         const blazeModelConfig = poseDetection.SupportedModels.BlazePose;
         poseDetection
@@ -87,11 +98,12 @@ function Test() {
             });
     }, []);
 
-
     function processResults(res) {
         //draw the skeleton on the canvas
         drawSkeleton(res)
-        console.log('var ', skeleton)
+        let res1, res2;
+        res1 = 1 + res.keypoints3D[2].y;
+        res2 = 1 + res.keypoints3D[5].y;
     }
 
 
@@ -128,7 +140,78 @@ function Test() {
             canvas.current.width = "0px";
             canvas.current.height = "0px";
         }
+        if (results.keypoints3D[26].score < 0.75) {
+            return;
+        }
 
+        //poses, left leg, right leg 
+        let standingPoses = [[180, 180], [90, 90], [180, 180]]
+        let curlPoses = [[180, 180], [90, 90], [180, 180]]
+
+        let rightKneeAngle;
+        rightKneeAngle = getAngle(results.keypoints3D[24], results.keypoints3D[26], results.keypoints3D[28]);
+
+        let leftKneeAngle;
+        leftKneeAngle = getAngle(results.keypoints3D[23], results.keypoints3D[25], results.keypoints3D[27]);
+
+        let leftElbowAngle;
+        leftElbowAngle = getAngle(results.keypoints3D[11], results.keypoints3D[13], results.keypoints3D[15]);
+
+        let rightElbowAngle;
+        rightElbowAngle = getAngle(results.keypoints3D[11], results.keypoints3D[13], results.keypoints3D[15]);
+
+        //stand
+        if (positionIndex == 3) {
+            setpositionIndex(0);
+            setstandReps(standReps + 1);
+        }
+
+        //curl
+        if (positionIndex2 == 3) {
+            setpositionIndex2(0);
+            setcurlReps(curlReps + 1);
+        }
+        //knees
+        if (((rightKneeAngle <= (standingPoses[positionIndex][0] + 35) && rightKneeAngle >= (standingPoses[positionIndex][0] - 35)) ||
+            (leftKneeAngle <= (standingPoses[positionIndex][1] + 35) && leftKneeAngle >= (standingPoses[positionIndex][1] - 35)))
+        ) {
+            setpositionIndex(positionIndex + 1);
+        }
+
+        //elbows
+        if (((rightElbowAngle <= (curlPoses[positionIndex2][0] + 35) && rightElbowAngle >= (curlPoses[positionIndex2][0] - 35)) ||
+            (leftElbowAngle <= (curlPoses[positionIndex2][1] + 35) && leftElbowAngle >= (curlPoses[positionIndex2][1] - 35)))
+        ) {
+            setpositionIndex2(positionIndex2 + 1);
+        }
+        console.log('pose ', curlPoses[positionIndex2])
+        console.log('index ', positionIndex2)
+        console.log('reps ', curlReps)
+        console.log('-------------')
+
+
+
+    }
+
+    function getAngle(vector1, vector2, vector3) {
+        let angle = 0;
+
+        let x1 = vector1.x, y1 = vector1.y, z1 = vector1.z;
+        let x2 = vector2.x, y2 = vector2.y, z2 = vector2.z;
+        let x3 = vector3.x, y3 = vector3.y, z3 = vector3.z;
+        //center x 
+        x1 = x1 - x2;
+        x3 = x3 - x2;
+        //center y
+        y1 = y1 - y2;
+        y3 = y3 - y2;
+        //center z
+        z1 = z1 - z2;
+        z3 = z3 - z2;
+
+        angle = Math.acos((x1 * x3 + y1 * y3 + z1 * z3) / (Math.sqrt((Math.pow(x1, 2) + Math.pow(y1, 2) + Math.pow(z1, 2))) * Math.sqrt(Math.pow(x3, 2) + Math.pow(y3, 2) + Math.pow(z3, 2))))
+        angle = (angle * (180 / Math.PI));
+        return angle;
     }
 
     //set skeleton to true o false tu 
@@ -143,6 +226,8 @@ function Test() {
             <h1>Movement Test</h1>
             <h2>Turn on Skeleton</h2>
             <input type="checkbox" name="vehicle1" value="Bike" id="skeletonCheckbox" />
+            <h2>Standing Reps: {standReps}</h2>
+            <h2>Curl Reps: {curlReps}</h2>
             <div>
                 {detector && (
                     <AILabWebCam
