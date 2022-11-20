f8d82a840c7e6d9c99f2e0bfbda43a99aafc59a7
inaki321 <inakihe00@gmail.com>
2022-11-19 18:36:06 -0600

    add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+//routes imports
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//pages imports 
 import Test from './pages/test';
+
 
 function App() {
   return (
-    <div className="App">
-      <Test />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h>hola </h>} />
+        <Route path="Test" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
